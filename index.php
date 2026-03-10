f07ebf5385cd36ed9a68869af261305a603fa5ba
Abhishek-Jadhav-2618 <abhijadhav2618@gmail.com>
2026-03-10 11:12:14 +0530

    Updated

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "<h1>PHP App Deployed and Updated with GitHub Actions + AWS!! Tested Successfully</h1>";
+echo "<h1>PHP App Deployed and Updated with GitHub Actions + AWS!! Testing Successfully</h1>";
 ?>
